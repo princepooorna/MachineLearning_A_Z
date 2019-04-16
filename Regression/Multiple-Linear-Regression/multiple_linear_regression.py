7564fa79886c86d6816729f56ab9fc4c252f2f21
princepooorna <45161044+princepooorna@users.noreply.github.com>
2019-04-16 19:38:00 +0530

    Update multiple_linear_regression.py
    The sklearn's 'train_test_split' have been moved to 'model_selection' .And You have not shown the plotting of graph which is difficult for beginners like me to understand this. The 'OneHotEncoder' is not necessary I guess, since the data is already in numbers.
    . So I updated this for you. Thank You.

--- Regression/Multiple-Linear-Regression/multiple_linear_regression.py
+++ Regression/Multiple-Linear-Regression/multiple_linear_regression.py
@@ -11,26 +11,19 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
-onehotencoder = OneHotEncoder(categorical_features = [3])
-X = onehotencoder.fit_transform(X).toarray()
 
-# Avoiding the Dummy Variable Trap
-X = X[:, 1:]
+
+#Preparing the dummies to divide the project data with respect to State values
+X_dummies = pd.get_dummies(X.State,prefix='State').iloc[:,1:]
+X = pd.concat([X.iloc[:,0:3],X_dummies],axis=1)
+print(X.head())
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting Multiple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
@@ -39,3 +32,8 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+#Plotting the result predicted data against the test data
+plt.plot(y_test,y_pred,'ro')
+plt.plot(y_test,y_test,'b-')
+plt.show()
